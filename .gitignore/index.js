5ac59fc8f64152a52fa7e44b1d8b58bf4630d1d1
France-Multigaming <53142230+France-Multigaming@users.noreply.github.com>
2019-08-21 00:37:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,36 +3,15 @@ const Discord = require("discord.js");
 // Client instance
 var client = new Discord.Client();
 
-//FS
-const fs = require('fs');
+// Config
+var prefix = "/";
 
-client.commands = new Discord.Collection();
-
-fs.readdir("./Commandes/", (error, f) => {
-    if(error) console.log(error);
-
-    let commandes = f.filter(f => f.split(".").pop() === "js");
-    if(commandes.length <= 0) return console.log("Aucune commande trouvé !");
-
-    commandes.forEach((f) => {
-
-        let commande = require(`./Commandes/${f}`);
-        console.log(`${f} commande chargée !`);
-
-    client.commands.set(commande.help.name, commande);
-    });
-});
-
-fs.readdir("./Events/", (error, f) => {
-    if(error) console.log(error);
-    console.log(`${f.length} event en chargement`);
-    f.forEach((f) => {
-        const events = require(`./Events/${f}`);
-        const event = f.split(".")[0];
-
-    client.on(event, events.bind(null, client));
-
-    });
+// Discord Status
+client.on("ready", function() {
+    client.user.setActivity('Discord', { type: 'WATCHING' })
+  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+  .catch(console.error);
+    console.log("Is online !");
 });
 
 // Welcome message
@@ -43,16 +22,9 @@ fs.readdir("./Events/", (error, f) => {
         .setDescription("Salut "+ user + "! Bienvenue dans la communauté de **France Multigaming** 🎉🤗 ! Afin de voir __***les salons de jeux***__ te correspondant, merci de te reporter dans <#584335406917746689> . Je t'invite aussi à prendre connaissance des <#584335354031636481> et <#584326401935671299> ainsi que les règles pour les jeux auxquels tu pourrais participer :innocent:. Bon jeu!")
         .setFooter("France Multigaming");
     user.guild.channels.get("584055404955303936").send(joinEmbed);
-
-var role = member.guild.roles.find('name', 'User');
-member.addRole(role)
-
 });
 
-// Config
-var prefix = "/";
-
-// Comands
+// Commands
 client.on("message", message=>{
     if (!message.guild) return
 
